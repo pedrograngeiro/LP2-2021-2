15f22f020dcb6aaa6be96596aa65594f0b2be084
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-09-19 22:16:08 -0300

    exercicio 6.1

---
+++ Exercicios 6.1/figures/Line.java
@@ -0,0 +1,27 @@
+package figures;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Line extends Figure{
+    private int x, y;
+    private int w, h;
+
+    public Line (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void print () {
+        System.out.format("A linha comeca no ponto (x %d, y %d) , termina no ponto (x2 %d, y2 %d) \n",
+            this.x, this.y, this.w, this.h);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawLine(this.x,this.y, this.w,this.h);
+    }
+}
